27ae8be826ead2eebf8be4965c4071004df05875
Mathieu Suchet <msuchet63@gmail.com>
2024-10-13 12:37:08 +0200

    Tests passed

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <catch2/catch_all.hpp>
 #include <TestsUtils.h>
+#include <Rewards/RewardsUtils.h>
+#include <Rewards/ZeroSumTests.h>
 
 TEST_CASE("Testing framework is working fine", "[Catch2]")
 {
@@ -9,3 +11,5 @@ TEST_CASE("Testing framework is working fine", "[Catch2]")
     REQUIRE(true);
 }
 
+TEST_CASE("Rewards", VOID_REWARDS_TAG){
+}
